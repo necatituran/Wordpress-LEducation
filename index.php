03af4a6685bd96b0063a9f4a9832d7c0c8028cf7
Necati Turan <hnecatti@gmail.com>
2023-08-27 03:37:33 +0300

    first functions

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+function greet($name,$color) {
+    echo "<p>Hi, my name is $name and $color is my fav color."    
+}
+    greet('Neco','Blue');
+
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php tag
