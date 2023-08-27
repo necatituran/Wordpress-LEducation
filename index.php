5148d404cfbcc579a0a16ee94e99d20191cf23b1
Necati Turan <hnecatti@gmail.com>
2023-08-27 04:03:18 +0300

    the_title,the_content,the_post also have_posts

--- index.php
+++ index.php
@@ -1,30 +1,10 @@
 <?php 
-function greet($name,$color) {
-    echo "<p>Hi, my name is $name and $color is my fav color.</p>";
-}
-    greet('Neco','Blue');
-    /*
-    $count = 1;
 
-    while($count <100){
-        echo "<li>$count</li>";
-        $count++;
-    } */
-    $count = 0;
-
-    $names = array('Neco','Hasan','Turan');
-    while($count < count($names)){
-        echo "<li>the names are : $names[$count]</li>";
-        $count++;
-    }
+    while(have_posts()){
+        the_post(); ?>
+        <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+        <?php the_content(); ?>
+        <hr>
+    <?php }
 
 ?>
-
-<h1><?php bloginfo('name'); ?></h1>        #site title
-<p><?php bloginfo('description');?></p>  //tagline
-<p>The names are  : <?php echo $names[0] ;?></p>  //arrays
-<p>The names are  : <?php echo $names[1] ;?></p> 
-<p>The names are  : <?php echo $names[2] ;?></p> 
-
-
-
