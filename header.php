e33ad8a7cac0f9f92dbfb0cbb3332c35b85e831e
Necati Turan <hnecatti@gmail.com>
2023-08-27 07:03:08 +0300

    from section 5

--- header.php
+++ header.php
@@ -1,1 +1,44 @@
-<h1>Greetings from header.php</h1>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html <?php language_attributes(); ?>>
+
+
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <header class="site-header">
+        <div class="container">
+            <h1 class="school-logo-text float-left">
+                <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
+            </h1>
+            <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search"
+                    aria-hidden="true"></i></span>
+            <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+            <div class="site-header__menu group">
+                <nav class="main-navigation">
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'theme-location' => 'headerMenuLocation'
+                        )
+                    )
+
+                        ?>
+                </nav>
+                <div class="site-header__util">
+                    <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+                    <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+                    <span class="search-trigger js-search-trigger"><i class="fa fa-search"
+                            aria-hidden="true"></i></span>
+                </div>
+            </div>
+        </div>
+    </header>
