9d275bcff202ced7c0c4378e3a2b427eb78228f5
Necati Turan <hnecatti@gmail.com>
2023-08-31 03:21:22 +0300

    program post types

--- university-post-types.php
+++ university-post-types.php
@@ -2,21 +2,45 @@
 
 function university_post_types()
 {
-    register_post_type('event', array(
-        'show_in_rest' => true,
-        'supports' => array('title', 'editor', 'excerpt'),
-        'rewrite' => array('slug' => 'events'),
-        'has_archive' => true,
-        'public' => true,
-        'labels' => array(
-            'name' => 'Events',
-            'add_new_item' => 'Add New Event',
-            'edit_item' => 'Edit Event',
-            'all_items' => 'All Events',
-            'singular_name' => 'Event'
-        ),
-        'menu_icon' => 'dashicons-calendar'
-    )
+    //Event Post Types
+    register_post_type(
+        'event',
+        array(
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor', 'excerpt'),
+            'rewrite' => array('slug' => 'events'),
+            'has_archive' => true,
+            'public' => true,
+            'labels' => array(
+                'name' => 'Events',
+                'add_new_item' => 'Add New Event',
+                'edit_item' => 'Edit Event',
+                'all_items' => 'All Events',
+                'singular_name' => 'Event'
+            ),
+            'menu_icon' => 'dashicons-calendar'
+        )
+    );
+
+    // Program Post Type
+
+    register_post_type(
+        'program',
+        array(
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor'),
+            'rewrite' => array('slug' => 'programs'),
+            'has_archive' => true,
+            'public' => true,
+            'labels' => array(
+                'name' => 'Programs',
+                'add_new_item' => 'Add New Program',
+                'edit_item' => 'Edit Program',
+                'all_items' => 'All Programs',
+                'singular_name' => 'Program'
+            ),
+            'menu_icon' => 'dashicons-awards'
+        )
     );
 }
 
