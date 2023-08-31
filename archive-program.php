58d5f718405dcfabddfcc4d3c0d33fc8fc536dff
Necati Turan <hnecatti@gmail.com>
2023-08-31 03:24:47 +0300

    program

--- archive-program.php
+++ archive-program.php
@@ -14,34 +14,16 @@ get_header(); ?>
 </div>
 
 <div class="container container--narrow page-section">
-    <?php
+    <ul class="link-list min-list">
+        <?php
 
-    while (have_posts()) {
-        the_post(); ?>
-        <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-                <span class="event-summary__month">
-                    <?php
-                    $eventDate = new DateTime(get_field('event_date'));
-                    echo $eventDate->format('M')
-                        ?>
-                </span>
-                <span class="event-summary__day">
-                    <?php echo $eventDate->format('d') ?>
-                </span>
-            </a>
-            <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p>
-                    <?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>"
-                        class="nu gray">Learn more</a>
-                </p>
-            </div>
-        </div>
-    <?php }
-    echo paginate_links();
-    ?>
-
+        while (have_posts()) {
+            the_post(); ?>
+            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+        <?php }
+        echo paginate_links();
+        ?>
+    </ul>
     <hr class="section-break">
 
     <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events') ?>">Check out our past events
